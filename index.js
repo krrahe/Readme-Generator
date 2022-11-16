cbea3f38c40c9cdfd6cc2d8bf6cf53a4c753ac3f
Riley Rahe <rilrahe@gmail.com>
2022-11-15 19:31:06 -0600

    commenting on code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+//consts for the required modules
 const inquirer = require("inquirer");
 const fs = require("fs");
-// TODO: Create an array of questions for user input
-const generateREADME = ({title, description, license,team,github}) => 
-`<h1 align="center">${title} 👋</h1>
+//outline for the questions to be inserted into the readme 
+const generateREADME = ({ title, description, license, team, github }) =>
+  `<h1 align="center">${title} 👋</h1>
   
 ![badge](https://img.shields.io/badge/license-${license}-brightgreen)<br />
 ## Description
@@ -20,41 +20,41 @@ This application is covered by the ${license} license.
 <br />
 Find me on GitHub: [${github}](https://github.com/${github})<br />
 `;
+//inquirer prompts for the readme
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the name of this project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What is the scope of this project?",
+    },
 
-
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "What is the name of this project?",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "What is the scope of this project?",
-  },
-
-  {
-    type: "list",
-    name: "license",
-    message: "Which license was used for this project?",
-    choices: ["MIT", "Apache", "GPL", "BSD", "Open", "None"],
-  },
-  {
-    type: "input",
-    name: "team",
-    message: "Who was involved in the development of this project?",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is your GitHub username?",
-  },
-])
-.then((answers) => {
-  const readMePageContent = generateREADME(answers);
-
-  fs.writeFile("README.md", readMePageContent, (err) =>
-    err ? console.log(err) : console.log("Successfully created README.md!")
-  );
-});
+    {
+      type: "list",
+      name: "license",
+      message: "Which license was used for this project?",
+      choices: ["MIT", "Apache", "GPL", "BSD", "Open", "None"],
+    },
+    {
+      type: "input",
+      name: "team",
+      message: "Who was involved in the development of this project?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?",
+    },
+  ])
+  .then((answers) => {
+    const readMePageContent = generateREADME(answers);
+//actually creates the readme file
+    fs.writeFile("README.md", readMePageContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created README.md!")
+    );
+  });
